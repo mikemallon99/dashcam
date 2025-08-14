e7f375dac2f45df70885768f7bc642df75009354
Mike Mallon <mikemallon99@gmail.com>
2025-08-13 21:51:28 -0500

    added ssh stuff

--- connect.sh
+++ connect.sh
@@ -1,1 +1,2 @@
-ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -oHostKeyAlgorithms=+ssh-rsa mikem@192.168.0.180
+# ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -oHostKeyAlgorithms=+ssh-rsa mikem@192.168.0.180
+ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -oHostKeyAlgorithms=+ssh-rsa mikem@172.20.10.4
--- watch.sh
+++ watch.sh
@@ -1,1 +1,5 @@
-vlc sftp://mikem@192.168.0.180:/home/mikem/projects/dashcam/_outputs/output.mkv
+# Sweetgreen WIFI
+# vlc sftp://mikem@192.168.0.180:/home/mikem/projects/dashcam/_outputs/output.mkv
+
+# iPhone Hotspot
+vlc sftp://mikem@172.20.10.4:/home/mikem/projects/dashcam/_outputs/output.mkv
